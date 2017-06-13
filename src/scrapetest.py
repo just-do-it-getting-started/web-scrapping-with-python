1d6423da46dea30a025f574e9a38116715fa2c22
KimJiTae <kimshinelove@gmail.com>
2017-06-13 13:42:14 +0900

    add bs4 module

--- src/scrapetest.py
+++ src/scrapetest.py
@@ -1,3 +1,5 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+bsObj = BeautifulSoup(html.read(), "html.parser")
+print(bsObj.h1)
