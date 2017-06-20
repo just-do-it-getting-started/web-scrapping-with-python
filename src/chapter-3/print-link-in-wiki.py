a6bd16ee297722be796890a86fdc71ed251523ad
KimJiTae <shine.kim@navercorp.com>
2017-06-20 13:39:20 +0900

    example: print all link

---
+++ src/chapter-3/print-link-in-wiki.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
+bsObj = BeautifulSoup(html, "html.parser")
+
+for link in bsObj.findAll("a"):
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
